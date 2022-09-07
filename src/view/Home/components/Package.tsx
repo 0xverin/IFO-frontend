949edd5e052204361ae09fe9a33bfa4855a6bd93
Verin1005 <daqingchong0809@gmail.com>
2022-09-07 15:56:21 +0800

    package

--- src/view/Home/components/Package.tsx
+++ src/view/Home/components/Package.tsx
@@ -1,6 +1,28 @@
-import { Box } from "@mui/material";
-
+/* eslint-disable @next/next/no-img-element */
+import { Box, Button } from "@mui/material";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import { useState } from "react";
 const WBox = (props: any) => {
+    const [radio, setRadio] = useState(1);
+    const iterms = [
+        {
+            key: 1,
+            src: "/assets/type1.png",
+            cost: 10,
+        },
+        {
+            key: 2,
+            src: "/assets/type2.png",
+            cost: 20,
+        },
+        {
+            key: 3,
+            src: "/assets/type3.png",
+            cost: 50,
+        },
+    ];
     return (
         <Box>
             <Box
@@ -12,27 +34,43 @@ const WBox = (props: any) => {
                     justifyContent: "space-between",
                 }}
             >
-                <Box
-                    sx={{
-                        height: "400px",
-                        width: "350px",
-                        bgcolor: "blue",
-                    }}
-                ></Box>
-                <Box
-                    sx={{
-                        height: "400px",
-                        width: "350px",
-                        bgcolor: "blue",
-                    }}
-                ></Box>
-                <Box
-                    sx={{
-                        height: "400px",
-                        width: "350px",
-                        bgcolor: "blue",
-                    }}
-                ></Box>
+                {iterms.map((item) => {
+                    return (
+                        <Box
+                            sx={{
+                                height: "420px",
+                                width: "350px",
+                            }}
+                            key={item.key}
+                            display="flex"
+                            justifyContent="center"
+                            flexWrap="wrap"
+                        >
+                            <img src={item.src} width={300} height={350} alt=""></img>
+                            <Box
+                                sx={{
+                                    width: "100%",
+                                }}
+                                display="flex"
+                                justifyContent="center"
+                            >
+                                <Button
+                                    variant="contained"
+                                    sx={{
+                                        height: "40px",
+                                        bgcolor: "#FFB154",
+                                        color: "black",
+                                        ":hover": {
+                                            bgcolor: "#FFA54A",
+                                        },
+                                    }}
+                                >
+                                    花 {item.cost}U 购买
+                                </Button>
+                            </Box>
+                        </Box>
+                    );
+                })}
             </Box>
             <Box
                 sx={{
@@ -44,13 +82,75 @@ const WBox = (props: any) => {
             >
                 <Box
                     sx={{
-                        height: "400px",
                         width: "90%",
-                        bgcolor: "blue",
                         m: "0 auto",
                     }}
                 >
-                    12313
+                    <RadioGroup
+                        row
+                        aria-labelledby="demo-row-radio-buttons-group-label"
+                        name="row-radio-buttons-group"
+                        value={radio}
+                        onChange={(e) => {
+                            setRadio(Number(e.target.value));
+                        }}
+                        sx={{
+                            m: "0 auto",
+                            display: "flex",
+                            justifyContent: "space-around",
+                        }}
+                    >
+                        <FormControlLabel
+                            value="1"
+                            control={<Radio />}
+                            label="中级盲盒"
+                            sx={{
+                                color: "#FFB154",
+                                "& .css-vqmohf-MuiButtonBase-root-MuiRadio-root.Mui-checked": {
+                                    color: "#FFB154",
+                                },
+                            }}
+                        />
+                        <FormControlLabel
+                            value="2"
+                            control={<Radio />}
+                            label="高级盲盒"
+                            sx={{
+                                color: "#FFB154",
+                                "& .css-vqmohf-MuiButtonBase-root-MuiRadio-root.Mui-checked": {
+                                    color: "#FFB154",
+                                },
+                            }}
+                        />
+                        <FormControlLabel
+                            value="3"
+                            control={<Radio />}
+                            label="特高级盲盒"
+                            sx={{
+                                color: "#FFB154",
+                                "& .css-vqmohf-MuiButtonBase-root-MuiRadio-root.Mui-checked": {
+                                    color: "#FFB154",
+                                },
+                            }}
+                        />
+                    </RadioGroup>
+                </Box>
+                <Box display="flex" justifyContent="center" flexWrap="wrap">
+                    <img src={iterms[radio - 1].src} width={300} height={350} alt=""></img>
+                    <Button
+                        variant="contained"
+                        sx={{
+                            height: "40px",
+                            bgcolor: "#FFB154",
+                            color: "black",
+                            my: "20px",
+                            ":hover": {
+                                bgcolor: "#FFA54A",
+                            },
+                        }}
+                    >
+                        花 {iterms[radio - 1].cost}U 购买
+                    </Button>
                 </Box>
             </Box>
         </Box>
@@ -59,17 +159,37 @@ const WBox = (props: any) => {
 
 export default function Package() {
     return (
-        <Box
-            sx={{
-                bgcolor: "white",
-                m: "0 auto",
-                width: {
-                    md: "65%",
-                    xs: "95%",
-                },
-            }}
-        >
-            <WBox></WBox>
+        <Box>
+            <Box
+                sx={{
+                    width: {
+                        md: "65%",
+                        xs: "95%",
+                    },
+                    m: "0 auto",
+                    textAlign: "center",
+                    color: "#FFB260",
+                    fontSize: {
+                        md: "32px",
+                        xs: "14px",
+                    },
+                    marginBottom: "20px",
+                }}
+            >
+                参与IFO获得盲盒，价格不同，获得NFT的几率也会不同
+            </Box>
+            <Box
+                sx={{
+                    bgcolor: "white",
+                    m: "0 auto",
+                    width: {
+                        md: "65%",
+                        xs: "95%",
+                    },
+                }}
+            >
+                <WBox></WBox>
+            </Box>
         </Box>
     );
 }
--- src/view/Home/index.tsx
+++ src/view/Home/index.tsx
@@ -7,6 +7,7 @@ export default function Home() {
     return (
         <Box>
             <Dashboard></Dashboard>
+
             <Package></Package>
             <NFT></NFT>
             <Inviter></Inviter>
